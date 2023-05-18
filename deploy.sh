576f2bc5abd8f8723030b9012dc0068df53c33c4
GautamR-Samagra <gautam@samagragovernance.in>
2023-05-18 15:10:15 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,9 @@ source venv/bin/activate
 pip install sshtunnel
 pip install numpy 
 pip install pandas 
-pip install logging
+
 pip install mysql-connector-python
+pip install  pymysql 
 
 python sql_pull_save_code.py
 python confidence_algo_run_script.py
